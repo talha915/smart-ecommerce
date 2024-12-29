6f7be80c6f204aed738937c07c05c756059206fd
talha915 <talhazafa915426@gmail.com>
2024-12-29 02:15:17 +0100

    popular products along with search bar UI is done

--- src/pages/products.js
+++ src/pages/products.js
@@ -1,19 +1,16 @@
 import Header from '@/components/Header';
+import PopularProducts from '@/components/PopularProducts';
 
 export default function Products() {
-  const products = [
-    { id: 1, name: 'iPhone 14', category: 'Phones', price: '$999', image: 'https://m.media-amazon.com/images/I/61BGE6iu4AL.jpg' },
-    { id: 2, name: 'MacBook Pro', category: 'Laptops', price: '$1999', image: '/macbookpro.jpg' },
-    { id: 3, name: 'iPhone 13', category: 'Phones', price: '$799', image: '/iphone13.jpg' },
-    { id: 4, name: 'Dell XPS 13', category: 'Laptops', price: '$1299', image: '/dellxps13.jpg' },
-    // More products can go here...
-  ];
 
   return (
     
     <div className="row">
       <div className="col-12">
         <Header />
+      </div>
+      <div className="container">
+          <PopularProducts />
       </div>
     </div> 
     
